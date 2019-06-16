83ae3b1ec786619b2d132d0a31dc2c14b435d181
Sonia <sonia.garrafan@gmail.com>
2019-06-16 02:06:15 -0500

    sexto

---
+++ WCFMedical/WCFAplicationsMedical/RegistrarAtencion.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace WCFAplicationsMedical
+{
+    partial class RegistrarAtencion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtHfin = new System.Windows.Forms.TextBox();
+            this.txtHinicio = new System.Windows.Forms.TextBox();
+            this.txtFecha = new System.Windows.Forms.TextBox();
+            this.txtObservacion = new System.Windows.Forms.TextBox();
+            this.txtMedico = new System.Windows.Forms.TextBox();
+            this.txtNumSolicitud = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumAtencion = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(393, 319);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 41);
+            this.button1.TabIndex = 29;
+            this.button1.Text = "&Registrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtHfin
+            // 
+            this.txtHfin.Location = new System.Drawing.Point(282, 279);
+            this.txtHfin.Name = "txtHfin";
+            this.txtHfin.Size = new System.Drawing.Size(58, 20);
+            this.txtHfin.TabIndex = 28;
+            // 
+            // txtHinicio
+            // 
+            this.txtHinicio.Location = new System.Drawing.Point(282, 249);
+            this.txtHinicio.Name = "txtHinicio";
+            this.txtHinicio.Size = new System.Drawing.Size(58, 20);
+            this.txtHinicio.TabIndex = 27;
+            // 
+            // txtFecha
+            // 
+            this.txtFecha.Location = new System.Drawing.Point(282, 215);
+            this.txtFecha.Name = "txtFecha";
+            this.txtFecha.Size = new System.Drawing.Size(100, 20);
+            this.txtFecha.TabIndex = 26;
+            // 
+            // txtObservacion
+            // 
+            this.txtObservacion.Location = new System.Drawing.Point(282, 184);
+            this.txtObservacion.Name = "txtObservacion";
+            this.txtObservacion.Size = new System.Drawing.Size(351, 20);
+            this.txtObservacion.TabIndex = 25;
+            // 
+            // txtMedico
+            // 
+            this.txtMedico.Location = new System.Drawing.Point(282, 151);
+            this.txtMedico.Name = "txtMedico";
+            this.txtMedico.Size = new System.Drawing.Size(100, 20);
+            this.txtMedico.TabIndex = 24;
+            // 
+            // txtNumSolicitud
+            // 
+            this.txtNumSolicitud.Location = new System.Drawing.Point(282, 121);
+            this.txtNumSolicitud.Name = "txtNumSolicitud";
+            this.txtNumSolicitud.Size = new System.Drawing.Size(100, 20);
+            this.txtNumSolicitud.TabIndex = 23;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(193, 279);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 13);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Hora Finaliza";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(204, 249);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Hora Inicia";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(167, 218);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Fecha de Atención";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(197, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Observación";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(219, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Medico";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(185, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "N° de Solicitud";
+            // 
+            // txtNumAtencion
+            // 
+            this.txtNumAtencion.Location = new System.Drawing.Point(282, 90);
+            this.txtNumAtencion.Name = "txtNumAtencion";
+            this.txtNumAtencion.Size = new System.Drawing.Size(100, 20);
+            this.txtNumAtencion.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(185, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "N° de Atencion";
+            // 
+            // RegistrarAtencion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtHfin);
+            this.Controls.Add(this.txtHinicio);
+            this.Controls.Add(this.txtFecha);
+            this.Controls.Add(this.txtObservacion);
+            this.Controls.Add(this.txtMedico);
+            this.Controls.Add(this.txtNumSolicitud);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNumAtencion);
+            this.Controls.Add(this.label1);
+            this.Name = "RegistrarAtencion";
+            this.Text = "RegistrarAtencion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtHfin;
+        private System.Windows.Forms.TextBox txtHinicio;
+        private System.Windows.Forms.TextBox txtFecha;
+        private System.Windows.Forms.TextBox txtObservacion;
+        private System.Windows.Forms.TextBox txtMedico;
+        private System.Windows.Forms.TextBox txtNumSolicitud;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumAtencion;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ WCFMedical/WCFAplicationsMedical/RegistrarAtencion.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.Serialization;
+using System.Web;
+using System.Data.SqlClient;
+using System.ServiceModel;
+
+namespace WCFAplicationsMedical
+{
+    public partial class RegistrarAtencion : Form
+    {
+        WCFServiceAtenciones.AtencionesClient obj = new WCFServiceAtenciones.AtencionesClient();
+        
+        public RegistrarAtencion()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WCFServiceAtenciones.Atencion objAtencion = new WCFServiceAtenciones.Atencion(); // add type reference
+
+            objAtencion.NunAtencion = Convert.ToInt32(txtNumAtencion.Text);
+            objAtencion.NumSolicitud = Convert.ToInt32(txtNumSolicitud.Text);
+            objAtencion.NumDni = Convert.ToInt32(txtMedico.Text);
+            objAtencion.Observacion = txtObservacion.Text;
+            objAtencion.FecRegistro = txtFecha.Text;
+            objAtencion.HoraInicio = txtHinicio.Text;
+            objAtencion.HoraFin = txtHfin.Text;
+
+
+            obj.CrearAtencion(objAtencion);
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RegistrarAtencion_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
